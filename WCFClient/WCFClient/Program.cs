55c6bb4772116a1d1f494ad6e185056cad00204b
stoilyanchev <stoilyanchev@gmail.com>
2015-10-06 16:53:21 +0300

    WCFServer

---
+++ WCFClient/WCFClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace WCFClient
+{
+    [ServiceContract]
+    public interface IStringReverser
+    {
+        [OperationContract]
+        string ReverseString(string value);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IStringReverser> httpFactory =
+              new ChannelFactory<IStringReverser>(
+                new BasicHttpBinding(),
+                new EndpointAddress(
+                  "http://localhost:8000/Reverse"));
+
+            ChannelFactory<IStringReverser> pipeFactory =
+              new ChannelFactory<IStringReverser>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress(
+                  "net.pipe://localhost/PipeReverse"));
+
+            IStringReverser httpProxy =
+              httpFactory.CreateChannel();
+
+            IStringReverser pipeProxy =
+              pipeFactory.CreateChannel();
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                Console.WriteLine("http: " +
+                    httpProxy.ReverseString(str));
+                Console.WriteLine("pipe: " +
+                    pipeProxy.ReverseString(str));
+            }
+        }
+    }
+}
---
+++ WCFServer/WCFServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCFServer
+{
+    [ServiceContract]
+    public interface IStringReverser
+    {
+        [OperationContract]
+        string ReverseString(string value);
+    }
+
+    public class StringReverser : IStringReverser
+    {
+        public string ReverseString(string value)
+        {
+            char[] returnValue = value.ToCharArray();
+            int index = 0;
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                returnValue[index++] = value[i];
+            }
+            return new string(returnValue);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(
+                typeof(StringReverser),
+                new Uri[] { new Uri("http://localhost:8000"), new Uri("net.pipe://localhost") }))
+            {
+                host.AddServiceEndpoint(typeof(IStringReverser), new BasicHttpBinding(), "Reverse");
+                host.AddServiceEndpoint(typeof(IStringReverser), new NetNamedPipeBinding(), "PipeReverse");
+
+                host.Open();
+
+                Console.WriteLine("Service is available." + "Press <ENTER> to exit.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
